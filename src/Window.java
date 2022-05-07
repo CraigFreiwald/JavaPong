956b6f196b6f29c3b1f3632f74ff59d5ed8233a7
craig <craig.freiwald@gmail.com>
2022-05-07 14:10:10 -0400

    Fixed class error.

--- src/Window.java
+++ src/Window.java
@@ -3,7 +3,6 @@ import javax.swing.*;
 public class Window extends JFrame implements Runnable{
     @Override
     public void run() {
-        Window window = new Window();
-        Thread t1 = new Thread();
+
     }
 }
